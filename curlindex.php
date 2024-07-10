401d7e54bb0463bb500486cd85a5b8debbb18c94
Sujeet <singhsujeet772@gmail.com>
2024-07-10 09:40:30 +0530

    curl initialization to use an api endpoint

---
+++ curlindex.php
@@ -0,0 +1,16 @@
+<?php
+
+$ch = curl_init();
+
+// curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt_array($ch,[
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true,
+]);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+echo $response,"\n";
