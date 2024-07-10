f712a2c3d77bad8a0d70d6887a733558259b2ff9
Sujeet <singhsujeet772@gmail.com>
2024-07-10 09:54:02 +0530

    used curl_getinfo() to fetch the status code

--- curlindex.php
+++ curlindex.php
@@ -5,12 +5,15 @@ $ch = curl_init();
 // curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt_array($ch,[
-    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=London&appid=7777fed14aa02ec38418591e6a4fd4e3",
     CURLOPT_RETURNTRANSFER => true,
 ]);
 
 $response = curl_exec($ch);
 
+$status_code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
+
 curl_close($ch);
 
+echo $status_code,"\n";
 echo $response,"\n";
