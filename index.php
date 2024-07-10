d04f5da7cd257b58fb329a7b2f79e8e4d30d7cc2
Sujeet <singhsujeet772@gmail.com>
2024-07-10 09:17:32 +0530

    used agify api and created a form to dynamic pass name and in response fetched age

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
-    $response = file_get_contents('https://randomuser.me/api');
+if(!empty($_GET['name'])){
+    $response = file_get_contents("https://api.agify.io/?name={$_GET['name']}");
     $data = json_decode($response,true);
 
-    echo $data['results'][0]['gender'];
+    $age = $data['age'];
+}
+    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if(isset($age)):?>
+        Age : <?= $age ?>
+
+    <?php endif; ?>
+    <form action="">
+        Name :<input type="text" name="name" id="name">
+        <button>Guess age</button>
+    </form>
+</body>
+</html>
