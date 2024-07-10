5406a21061032b57a57e2333eee393f0dd5fc73b
Sujeet <singhsujeet772@gmail.com>
2024-07-10 11:33:15 +0530

    used git access token passing in headers or CURLOPT_USERAGENT

--- curlindex.php
+++ curlindex.php
@@ -3,40 +3,40 @@
 $ch = curl_init();
 
 $headers = [
-    "Authorization: Client-ID 8JRk7nea5HoqJ5Wu9zhGtPw4yUA-adrVXunyrmx3KWg"
+    "Authorization: token YOUR_ACCESS_KEY"
+    // "User-Agent: singhsujeet772"
 ];
 
-$respons_headers = [];
+// $respons_headers = [];
 
-$header_callback = function($ch, $headers) use (&$respons_headers){
-    $len = strlen($headers);
-    $parts = explode(";", $headers, 2);
-    if(count($parts) < 2){
-        return $len;
-    }
-    $respons_headers[$parts[0]] = trim($parts[1]);
-    return $len;
-};
+// $header_callback = function($ch, $headers) use (&$respons_headers){
+//     $len = strlen($headers);
+//     $parts = explode(";", $headers, 2);
+//     if(count($parts) < 2){
+//         return $len;
+//     }
+//     $respons_headers[$parts[0]] = trim($parts[1]);
+//     return $len;
+// };
 
 // curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt_array($ch,[
-    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_URL => "https://github.com/singhsujeet772/php-rest-api",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADER => true,
-    CURLOPT_HEADERFUNCTION => $header_callback
+    CURLOPT_USERAGENT => "SINGHSUJEET772"
 ]);
 
 $response = curl_exec($ch);
 
-// $status_code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
+$status_code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
 // $content_type = curl_getinfo($ch , CURLINFO_CONTENT_TYPE);
 // $content_length = curl_getinfo($ch , CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($ch);
 
-// echo $status_code,"\n";
+echo $status_code,"\n";
 // echo $content_type,"\n";
 // echo $content_length,"\n";
-echo $response,"\n";
+// echo $response,"\n";
